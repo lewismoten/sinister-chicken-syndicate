35117e88d3af18b09a2f0f5ff4e6eecd6bb778b8
Lewis Moten <lewismoten@gmail.com>
2016-05-01 11:08:21 -0400

    gulp test for ci

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,18 @@
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
+    istanbul = require('gulp-istanbul'),
+    jasmine = require('gulp-jasmine'),
     config = require('./package.json');
 
   gulp.task('build', buildJs);
+  gulp.task('test', ['pre-test'], test);
+
+  gulp.task('pre-test', preTest);
 
   function buildJs() {
 
-    gulp
+    return gulp
       .src([
         'src/index.js',
         'src/**/*.js'
@@ -22,6 +27,23 @@
       .pipe(concat(config.main))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('.'));
+
+  }
+
+  function preTest() {
+
+    return gulp.src(['src/**/*.js'])
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire());
+
+  }
+
+  function test() {
+
+    return gulp.src(['spec/**/*-spec.js'])
+      .pipe(jasmine())
+      .pipe(istanbul.writeReports())
+      .pipe(istanbul.enforceThresholds({thresholds: {global: 90}}));
 
   }
 
